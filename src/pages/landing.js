3ef315948b92bf2b2739a2e301dffe671caa8ee8
Jon-Michael <narvaez.jonmichael@gmail.com>
2021-08-20 08:29:32 -0500

    first commit

---
+++ src/pages/landing.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Landing() {
+  return (
+    <div
+      style={{ width: "100%", height: "100%", backgroundColor: "#171717" }}
+    ></div>
+  );
+}
